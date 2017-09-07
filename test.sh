7362e320ecfa22e666624150a59a9808bc8aa9e6
gary-yan <383791970@qq.com>
2017-09-07 22:32:48 +0200

    small change

--- test.sh
+++ test.sh
@@ -21,30 +21,41 @@ while getopts n:c2rftw opt ;do
 	# cat thttpd.log|awk '{print $1}'|sort|uniq|wc – l
 	 # awk '{print $1}' thttpd.log
 	 # cat thttpd.log|awk '{print $1}'|sort|uniq
-	 cat thttpd.log|awk '{print $1}' | sort | uniq -c | sort -k1nr | head -10
+	  cat thttpd.log|awk '{print $1}' | sort | uniq -c | sort -k1nr | head -10
 	 #sort -k1nr  k--表示对第k列进行排序 n－－表示按数值排列  r--表示逆向排列  
 	 #uniq -c uniq 命令删除文件中的重复行。 -c 在输出行前面加上每行在输入文件中出现的次数。
 
 	;;
 	2)
 	#echo"Which address makes the most number of successful attempts?"
-	cat thttpd.log | awk '($9 ~ /200/)' thttpd.log | awk '{print $1}' | sort | uniq -c | sort -k1nr | head -10
+	cat thttpd.log | awk '($9 ~ /200/)' | awk '{print $1}' | sort | uniq -c | sort -k1nr | head -10
 	#统计所有状态码为200的请求
 	;;
 	r)
 # 	echo"What are the most common results codes and where do they come from?"
 	# cat thttpd.log | awk '{print $9}' | sort |uniq -c | sort -k1nr 
-	cat thttpd.log | awk '{print $9}'| sort | uniq -c | sort -k1nr | head -10
+	cat thttpd.log | awk '{print $9}'| sort | uniq -c | sort -k1nr | head -1 
 	#统计出现最多的状态码
+	cat thttpd.log | awk '($9 ~ /404/)'| sort | awk '{print $1}' | sort | uniq -c | sort -k1nr | head -10
 	;;
 	f)
 	echo"What are the most common result codes that indicate failure (no
 auth, not found etc) and where do they come from?"
 	;;
 	t)
-	echo"Which IP number get the most bytes sent to them?"
+	# echo"Which IP number get the most bytes sent to them?"
+	cat thttpd.log | awk '{print $10}' thttpd.log | awk '{print $1}' | sort -nr | head -5  
+	#cat thttpd.log | awk '($10 | sort -nr | head -5)' | awk '{print $1}'
+		#thttpd.log| awk '{print $1}' | sort | uniq -c | sort -k1nr| head -10
 	;;
 	w)
+	 cat thttpd.log | awk '{print $9}' | sort |uniq -c | sort -k1nr 
+	 cat thttpd.log | awk '($10 ~ /17401502/)' | awk '{print $1}' | sort | uniq -c | sort -k1nr 
+
+
+
+	 ;;
+
 	
 	esac
 done
